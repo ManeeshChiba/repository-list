1f8e3b6647f64f17b9d6f13a21104f6270fdbead
Maneesh <mpchiba@gmail.com>
2021-02-02 18:29:43 -0500

    Added background

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-function App() {
+import React from 'react';
+import styles from './App.module.scss';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className={styles.layout}>
+      test
+    </main>
   );
-}
+};
 
 export default App;
